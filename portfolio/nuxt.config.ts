28340f281cbc27411da31856be5489f2e9c6960a
Neil Lin <neillin@blueplanet.com.tw>
2022-04-15 22:49:33 +0800

    test

--- portfolio/nuxt.config.ts
+++ portfolio/nuxt.config.ts
@@ -6,6 +6,6 @@ export default defineNuxtConfig({
     '@/assets/scss/style.scss',
   ],
   router: {
-    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/portfolio/dist/' : '/'
+    base: process.env.DEPLOY_ENV === 'GH_PAGES' ? '/portfolio/output/' : '/'
   }
 })
