aae0bdd47c7b7bbe238c8a2a2c7b549a3cde05ee
Neil Lin <neillin@blueplanet.com.tw>
2022-05-20 23:46:31 +0800

    feature: add scout
    feature: add scout

--- _nuxt/static/1653036894/works/mpos/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1653036894",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fmpos",config:{baseURL:"http:\u002F\u002Flocalhost:3000",_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
---
+++ _nuxt/static/1653061551/works/scout/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/works/scout", {data:[{}],fetch:{},mutations:void 0});
