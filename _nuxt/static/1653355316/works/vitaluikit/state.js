811b0946c95a0323a8abe0c6994a2df4e004766d
Neil-Lin <neillin@blueplanet.com.tw>
2022-05-24 09:22:46 +0800

    feature: add clarity & Google optimize

---
+++ _nuxt/static/1653355316/works/vitaluikit/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1653355316",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fvitaluikit",config:{baseURL:"http:\u002F\u002Flocalhost:3000",_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
