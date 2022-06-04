ef2c1e0bd5e97282c9451631ff75dde77627f60c
Neil Lin <neillin@blueplanet.com.tw>
2022-06-04 17:30:21 +0800

    fix: about

---
+++ _nuxt/static/1654334975/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002F","\u002Fworks\u002Fvitaluikit","\u002Fworks\u002Fvitalcrm-app","\u002Fworks\u002Fpiman","\u002Fworks\u002Fscout","\u002Fworks\u002Fvitalsespsurvey","\u002Fworks\u002Fgeneral","\u002Fworks\u002Fmpos"]})
