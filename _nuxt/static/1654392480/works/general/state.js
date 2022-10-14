55fba4359ab71f59185a0d6c94932036996b6777
Neil Lin <neillin@blueplanet.com.tw>
2022-10-14 23:00:19 +0800

    fix: piman project content

--- _nuxt/static/1654392480/works/general/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1654392480",layout:"default",error:a,serverRendered:true,routePath:"\u002Fworks\u002Fgeneral",config:{baseURL:"http:\u002F\u002Flocalhost:3000",_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a}},globalRefs:{}}}(null));
