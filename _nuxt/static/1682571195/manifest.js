9c856df0b5fad5083f26a363c500bcbf5f66bbb3
Neil-Lin <neillin1023@gmail.com>
2023-04-27 12:53:58 +0800

    fix: link

---
+++ _nuxt/static/1682571195/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fworks\u002Fchoose-chart","\u002Fabout","\u002F","\u002Fworks\u002Fgeneral","\u002Fworks\u002Fscout","\u002Fworks\u002Fvitalcrm-app","\u002Fworks\u002Fmpos","\u002Fworks\u002Fvitaluikit","\u002Fworks\u002Fpiman","\u002Fworks\u002Fvitalsespsurvey"]})
