e52ce07892135019398efc0c077db2a35c97e541
Neil-Lin <neillin1023@gmail.com>
2023-04-27 12:40:53 +0800

    fix: piman link

---
+++ _nuxt/static/1682570321/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fworks\u002Fchoose-chart","\u002Fworks\u002Fgeneral","\u002Fworks\u002Fmpos","\u002Fworks\u002Fpiman","\u002Fworks\u002Fvitalsespsurvey","\u002Fworks\u002Fscout","\u002Fworks\u002Fvitalcrm-app","\u002Fabout","\u002Fworks\u002Fvitaluikit"]})
