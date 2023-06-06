219bd9a94c74ae991ec6b291154d604faac4eeca
Neil-Lin <neillin1023@gmail.com>
2023-06-06 14:53:39 +0800

    feat: init nuxt3 form paprika template

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="page page--index">
+    <nuxt-link
+      id="ak-container"
+      to="#ak-container"
+      title="中央內容區塊"
+      accesskey="C"
+      name="ak-container"
+    >
+      :::
+    </nuxt-link>
+    <h2>{{ pageTitle }}</h2>
+    <theEmptyContent>Empty</theEmptyContent>
+  </main>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig()
+const pageTitle = ref('Home Page')
+
+useHead({
+  title: pageTitle,
+  meta: [
+    {
+      hid: 'og:title',
+      property: 'og:title',
+      content: pageTitle.value + ' - ' + runtimeConfig.public.websiteName
+    },
+    {
+      hid: 'twitter:title',
+      name: 'twitter:title',
+      content: pageTitle.value + ' - ' + runtimeConfig.public.websiteName
+    }
+  ]
+})
+</script>
