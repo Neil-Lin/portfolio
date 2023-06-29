ac04a4e97e088bc74fd18286a6f98a5993a04ae7
Neil-Lin <neillin1023@gmail.com>
2023-06-29 18:56:38 +0800

    Updates

---
+++ _nuxt/akContainer.26212b8f.js
@@ -0,0 +1,1 @@
+import{_ as n}from"./nuxt-link.9ebd3538.js";import{_ as o}from"./_plugin-vue_export-helper.c27b6911.js";import{o as e,c,w as _,k as a}from"./entry.135f683c.js";const r={};function s(i,m){const t=n;return e(),c(t,{id:"ak-container",to:"#ak-container",title:"中央內容區塊",accesskey:"C",name:"ak-container"},{default:_(()=>[a(" ::: ")]),_:1})}const x=o(r,[["render",s]]);export{x as _};
