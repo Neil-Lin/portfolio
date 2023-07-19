4a609a84a38ce957994a4e67687ab87bcb493930
Neil-Lin <neillin1023@gmail.com>
2023-07-19 20:20:53 +0800

    Updates

--- _nuxt/i18n.options.5c102ef0.js
+++
@@ -1,1 +0,0 @@
-import{R as i,S as a,T as n}from"./entry.9502a306.js";async function s(){const t=await i(()=>import("./i18n.config.2d2f2de3.js"),[],import.meta.url).then(o=>o.default||o);return a(t)?await t():n(t)?t:{}}export{s as default};
---
+++ _nuxt/i18n.options.f21b593c.js
@@ -0,0 +1,1 @@
+import{R as i,S as a,T as n}from"./entry.de926d11.js";async function s(){const t=await i(()=>import("./i18n.config.2d2f2de3.js"),[],import.meta.url).then(o=>o.default||o);return a(t)?await t():n(t)?t:{}}export{s as default};
