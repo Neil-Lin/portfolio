530a50a44d2781b752e103de98f0c5bbedca2ad0
Neil Lin <neillin@blueplanet.com.tw>
2024-04-13 00:48:15 +0800

    fix: add focus status to accesskey when refresh page

--- components/akContainer.vue
+++ components/akContainer.vue
@@ -1,5 +1,6 @@
 <template>
   <a
+    ref="focusableLink"
     id="ak-container"
     href="#ak-container"
     :title="$t('shortcut.mainContent')"
@@ -8,3 +9,13 @@
     :::
   </a>
 </template>
+
+<script setup lang="ts">
+const focusableLink = ref<HTMLAnchorElement | null>(null);
+
+onMounted(() => {
+  if (focusableLink.value) {
+    focusableLink.value.focus();
+  }
+});
+</script>
