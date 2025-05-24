3035c9b4c460801e4e061790c2bbdd9d09c91a5f
Neil-Lin <neillin1023@gmail.com>
2025-05-24 19:46:13 +0800

    Updates

---
+++ _nuxt/C7tk0Hlk.js
@@ -0,0 +1,1 @@
+const s=()=>({legacy:!1,locale:"zh-Hant-TW",fallbackLocale:["en","zh-Hant-TW"],messages:(()=>{var e,n;const a=Object.assign({});return{"zh-Hant-TW":((e=a["/lang/zh-Hant-TW.json"])==null?void 0:e.default)??{},en:((n=a["/lang/en-US.json"])==null?void 0:n.default)??{}}})()});export{s as default};
